b45ce84a11962bb3b3dbe604deb46b2ccafa7723
gilav21 <gilav21@gmail.com>
2022-04-28 17:52:14 +0300

    change

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
-
-
+import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
